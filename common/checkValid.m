8a554cc0330b37e40d2274d15325cea6ed363250
Kai KANG <myfavouritekk@gmail.com>
2015-05-03 13:58:36 +0800

    Add help messages for checkValid and threshold.

--- common/checkValid.m
+++ common/checkValid.m
@@ -1,4 +1,9 @@
 function [] = checkValid(infile, var_name)
+%%CHECKVALID check if an element in a Mat-file does not contain NaN or Inf
+%
+%   CHECKVALID(INFILE, VAR_NAME) checks if element VAR_NAME in Mat-file
+%   INFILE does not contain NaN or Inf
+%
 var = variable(var_name, infile);
 if any(isnan(var(:)))
     sprintf('%s has NaN.\n', infile);
--- common/threshold.m
+++ common/threshold.m
@@ -1,5 +1,10 @@
 function [] = threshold(infile, outfile, var_name, thres_value)
 %%THRESHOLD: set thresholds on a variable in a matfile
+%
+%   THRESHOLD(INFILE, OUTFILE, VAR_NAME, THRES_VALUE) sets thresold value
+%   THRES_VALUE on element VAR_NAME in INFILE and save the result in OUTFILE
+%   with the same variable name VAR_NAME
+%
     mat = variable(var_name, infile);
     mask = mat>thres_value;
     mat(mask) = 1;
